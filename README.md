7d1be3a49139b3ff63d51222406c095d296a2a8a
LEE <wiolem@hotmail.com>
2020-10-14 22:30:02 +0800

    :art: fix code

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# create-starter-app
+# create-starter
+
+[![Build Status][travis-img]][travis-url]
+[![Coverage Status][codecov-img]][codecov-url]
+[![NPM Downloads][downloads-img]][downloads-url]
+[![NPM Version][version-img]][version-url]
+[![License][license-img]][license-url]
+[![Dependency Status][dependency-img]][dependency-url]
+[![devDependency Status][devdependency-img]][devdependency-url]
+[![Code Style][style-img]][style-url]
+
+> Awesome starter.
+
+## Installation
+
+Use npx:
+
+```shell
+$ npx create-starter init [options]
+```
+
+Globally install:
+
+```shell
+$ npm install create-starter -g
+# or yarn
+$ yarn global add create-starter
+```
+
+## Usage
+
+```shell
+$ npm init starter init
+npx: installed 63 in 4.701s
+
+   ┌───────────────────────────────────────────┐    
+   │                                           │    
+   │   Welcome use starter to init your app.   │    
+   │                                           │    
+   └───────────────────────────────────────────┘    
+
+? Please select the app type Vue 3
+? Please select an app vite-tailwind-starter
+? Please input the app name vite-tailwind-starter   
+? Please input the app description my-vite-app      
+? Please input the app version 1.0.1
+? Please input the app author Lee
+
+   ┌───────────────────────────┐
+   │                           │
+   │   Starter init success!   │
+   │                           │
+   └───────────────────────────┘
+
+ cd ./vite-tailwind-starter
+ npm install
+
+```
+
+## Todos
+
+- [ ] Support templates cache & update
+- [ ] Support analysis package.json & replace
+
+## Contributing
+
+1. **Fork** it on GitHub!
+2. **Clone** the fork to your own machine.
+3. **Checkout** your feature branch: `git checkout -b my-awesome-feature`
+4. **Commit** your changes to your own branch: `git commit -am 'Add some feature'`
+5. **Push** your work back up to your fork: `git push -u origin my-awesome-feature`
+6. Submit a **Pull Request** so that we can review your changes.
+
+> **NOTE**: Be sure to merge the latest from "upstream" before making a pull request!
+
+## License
+
+[MIT](LICENSE) &copy; [LEE](https://wiolem.github.com)
+
+
+
+[travis-img]: https://img.shields.io/travis/wiolem/create-starter
+[travis-url]: https://travis-ci.org/wiolem/create-starter
+[codecov-img]: https://img.shields.io/codecov/c/github/wiolem/create-starter
+[codecov-url]: https://codecov.io/gh/wiolem/create-starter
+[downloads-img]: https://img.shields.io/npm/dm/create-starter
+[downloads-url]: https://npmjs.org/package/create-starter
+[version-img]: https://img.shields.io/npm/v/create-starter
+[version-url]: https://npmjs.org/package/create-starter
+[license-img]: https://img.shields.io/github/license/wiolem/create-starter
+[license-url]: https://github.com/wiolem/create-starter/blob/master/LICENSE
+[dependency-img]: https://img.shields.io/david/wiolem/create-starter
+[dependency-url]: https://david-dm.org/wiolem/create-starter
+[devdependency-img]: https://img.shields.io/david/dev/wiolem/create-starter
+[devdependency-url]: https://david-dm.org/wiolem/create-starter?type=dev
+[style-img]: https://img.shields.io/badge/code_style-standard-brightgreen
+[style-url]: https://standardjs.com
--- bin/create-starter.js
+++ bin/create-starter.js
@@ -1,125 +1,126 @@
 #!/usr/bin/env node
 
-const path = require("path");
-const cli = require("cac")();
-const fse = require("fs-extra");
-const inquirer = require("inquirer");
-const Git = require("simple-git")();
-const Spinner = require("cli-spinner").Spinner;
-const marked = require("marked");
-const boxen = require("boxen");
+const path = require('path')
+const cli = require('cac')()
+const fse = require('fs-extra')
+const inquirer = require('inquirer')
+const Git = require('simple-git')()
+const Spinner = require('cli-spinner').Spinner
+const marked = require('marked')
+const boxen = require('boxen')
+const { TEMPLATE_URL } = require('../lib/const')
 
 cli
-  .command("init", "init an app")
-  .option("--type [type]", "choose a project type")
+  .command('init', 'init an app')
+  .option('--type [type]', 'choose a project type')
   .action(async (options) => {
     console.log(
-      boxen(`Welcome use starter to init your app.`, { padding: 1, margin: 1 })
-    );
-    const spinner = new Spinner("Fetching starters .. %s");
-    spinner.setSpinnerString("/-\\");
-    spinner.start();
-    const tempPath = path.join(__dirname, ".temp");
-    await fse.remove(tempPath);
+      boxen('Welcome use starter to init your app.', { padding: 1, margin: 1 })
+    )
+    const spinner = new Spinner('Fetching starters .. %s')
+    spinner.setSpinnerString('/-\\')
+    spinner.start()
+    const tempPath = path.join(__dirname, '.temp')
+    await fse.remove(tempPath)
     await Git.clone(
-      `https://github.com/fe-starter/awesome-starter.git`,
+      TEMPLATE_URL,
       tempPath
-    );
-    spinner.stop(true);
-    const readme = await fse.readFile(path.join(tempPath, "README.md"), "utf8");
-    const tokens = marked.lexer(readme);
-    let start = false;
-    let result = {};
-    let current;
+    )
+    spinner.stop(true)
+    const readme = await fse.readFile(path.join(tempPath, 'README.md'), 'utf8')
+    const tokens = marked.lexer(readme)
+    let start = false
+    const result = {}
+    let current
     tokens.forEach((token) => {
-      if (token.type === "heading" && token.text === "Templates") {
-        start = true;
-        return;
+      if (token.type === 'heading' && token.text === 'Templates') {
+        start = true
+        return
       }
       if (start) {
-        if (token.type === "heading" && token.depth === 4) {
-          current = token.text;
+        if (token.type === 'heading' && token.depth === 4) {
+          current = token.text
         }
-        if (token.type === "list") {
+        if (token.type === 'list') {
           const items = token.items.map(({ text }) => {
-            const [name, link, description] = text.match(/\[.+\]|\(.+\)|.+/g);
+            const [name, link, description] = text.match(/\[.+\]|\(.+\)|.+/g)
             return {
-              name: name.replace(/\[|\]/g, "").trim(),
-              link: link.replace(/\(|\)/g, "").trim() + ".git",
-              description: description.replace(/\-/g, "").trim(),
-            };
-          });
-          result[current] = items;
+              name: name.replace(/\[|\]/g, '').trim(),
+              link: link.replace(/\(|\)/g, '').trim() + '.git',
+              description: description.replace(/-/g, '').trim()
+            }
+          })
+          result[current] = items
         }
       }
-      if (token === "heading" && token.depth !== 4) {
-        start = false;
+      if (token === 'heading' && token.depth !== 4) {
+        start = false
       }
-    });
+    })
     const { type } = await inquirer.prompt([
       {
-        type: "list",
-        name: "type",
+        type: 'list',
+        name: 'type',
         choices: Object.keys(result),
-        message: "Please select the app type",
-      },
-    ]);
-    const repos = result[type];
+        message: 'Please select the app type'
+      }
+    ])
+    const repos = result[type]
     const { temp } = await inquirer.prompt([
       {
-        type: "list",
-        name: "temp",
+        type: 'list',
+        name: 'temp',
         choices: repos.map(({ name }) => name),
-        message: "Please ...",
-      },
-    ]);
-    const repo = repos.find(({ name }) => name === temp);
+        message: 'Please select an app'
+      }
+    ])
+    const repo = repos.find(({ name }) => name === temp)
     const answer = await inquirer.prompt([
       {
-        type: "input",
-        name: "name",
+        type: 'input',
+        name: 'name',
         default: repo.name,
-        message: "Please input the app name",
+        message: 'Please input the app name'
       },
       {
-        type: "input",
-        name: "description",
-        message: "Please input the app description",
+        type: 'input',
+        name: 'description',
+        message: 'Please input the app description'
       },
       {
-        type: "input",
-        name: "version",
-        message: "Please input the app version",
+        type: 'input',
+        name: 'version',
+        message: 'Please input the app version'
       },
       {
-        type: "input",
-        name: "author",
-        message: "Please input the app author",
-      },
-    ]);
-    spinner.setSpinnerTitle("clone processing .. %s");
-    spinner.start();
-    const packageName = `./${answer.name}`;
-    await fse.remove(packageName);
-    await Git.clone(repo.link, packageName);
-    await fse.remove(path.resolve(packageName, "./.git"));
+        type: 'input',
+        name: 'author',
+        message: 'Please input the app author'
+      }
+    ])
+    spinner.setSpinnerTitle('clone processing .. %s')
+    spinner.start()
+    const packageName = `./${answer.name}`
+    await fse.remove(packageName)
+    await Git.clone(repo.link, packageName)
+    await fse.remove(path.resolve(packageName, './.git'))
     const file = await fse.readFile(
-      path.resolve(packageName, "./package.json"),
-      "utf8"
-    );
-    const pkg = file ? JSON.parse(file) : {};
-    const tempStr = JSON.stringify(Object.assign(pkg, answer), null, 2);
+      path.resolve(packageName, './package.json'),
+      'utf8'
+    )
+    const pkg = file ? JSON.parse(file) : {}
+    let tempStr = JSON.stringify(Object.assign(pkg, answer), null, 2)
     if (!file) {
-      tempStr = JSON.stringify(answer, null, 2);
+      tempStr = JSON.stringify(answer, null, 2)
     }
     await fse.writeFile(
-      path.resolve(packageName, "./package.json"),
+      path.resolve(packageName, './package.json'),
       tempStr,
-      "utf8"
-    );
-    spinner.stop(true);
-    console.log(boxen(`Starter init success!`, { padding: 1, margin: 1 }));
-    console.log(` cd ${packageName}\n npm install \n`);
-  });
+      'utf8'
+    )
+    spinner.stop(true)
+    console.log(boxen('Starter init success!', { padding: 1, margin: 1 }))
+    console.log(` cd ${packageName}\n npm install \n`)
+  })
 
-cli.help().parse();
+cli.help().parse()
